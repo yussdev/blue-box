7359ba26ff78dd90a35c57b5975f24f6b34c9c8e
Youssef <yussdev@gmail.com>
2023-03-08 19:57:46 +0330

    fix(apis): using axios

--- src/services/app.js
+++ src/services/app.js
@@ -5,15 +5,11 @@ export const appApi = {
     return apiClient.get("/app/settings").then((res) => res.data);
   },
   getTableData() {
-    return fetch("https://paul.blueboxonline.com/api/v1/app/tabledata", {
-      method: "GET",
-      credentials: "include",
-    });
-    // return apiClient
-    //   .get("/app/tabledata", {
-    //     withCredentials: true,
-    //   })
-    //   .then((res) => res.data);
+    return apiClient
+      .get("/app/tabledata", {
+        withCredentials: true,
+      })
+      .then((res) => res.data);
   },
   postReport(formData) {
     return apiClient.get("/app/tabledata", formData).then((res) => res.data);
--- src/services/auth.js
+++ src/services/auth.js
@@ -5,13 +5,9 @@ export const authApi = {
     return apiClient.get("/users/session").then((res) => res.data);
   },
   login(payload) {
-    return fetch("https://paul.blueboxonline.com/api/v1/users/login", {
-      method: "POST",
-      body: JSON.stringify(payload),
-      mode: "navigate",
-      credentials: "include",
-    });
-    // return apiClient.post("/users/login", payload).then((res) => res.data);
+    return apiClient
+      .post("/users/login", payload, { withCredentials: true })
+      .then((res) => res.data);
   },
   logout() {
     return apiClient.get("/users/logout").then((res) => res.data);
